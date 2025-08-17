b7c0dd3bce362054ef5aa84e5dad3fe34a4466e8
윤석 이 <nuclearbomb280@gmail.com>
2025-08-17 20:38:16 +0900

    feature : added unit and monster type enums

---
+++ CodeStudy_homework/CodeStudy_homework/Characters/eMonsterTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeStudy_homework_1.Characters
+{
+    public enum eMonsterTypes
+    {
+        SLIME = 1,
+        SKELETON = 2,
+        OAK = 4
+    }
+}
