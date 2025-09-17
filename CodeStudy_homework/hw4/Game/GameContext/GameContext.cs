180433db1573e78469cadbc820e93bd9fad4bdd9
윤석 이 <nuclearbomb280@gmail.com>
2025-09-17 22:32:30 +0900

    게임 상태 관리용 클래스 제작

---
+++ CodeStudy_homework/hw4/Game/GameContext/GameContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4.Game.GameContext
+{
+    public class GameContext
+    {
+        private int _cnt = 0;
+        public bool SkipInfo {  get; set; }
+        public int GameLoopCnt { get { return _cnt; }}
+        public ConsoleKeyInfo PressedKeyInfo { get; set; }
+        public void ResetLoopCnt()
+        {
+            _cnt = 0;
+        }
+        public void MoveToNextTurn()
+        {
+            _cnt++;
+        }
+    }
+}
---
+++ CodeStudy_homework/hw4/Game/GameContext/eGameStates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4.Game.GameContext
+{
+    public enum eGameStates
+    {
+        GAME_INITIAL_SCREEN = 1,
+        UNIT_TURN = 2,
+        MONSTER_TURN=4,
+        GAME_END=8
+    }
+}
