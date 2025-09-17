773c254cc8407aea72ab1865eab924756e47ee9e
윤석 이 <nuclearbomb280@gmail.com>
2025-09-17 23:36:48 +0900

    InputManager추가

---
+++ CodeStudy_homework/hw4/Game/GameManager/InputManagerComponent.cs
@@ -0,0 +1,31 @@
+﻿using hw4.Engine.Component;
+using hw4.Engine.Core.KeyEvent;
+
+namespace hw4.Game.GameManager
+{
+    public class InputManagerComponent : ComponentBehaviour
+    {
+        public GameContext.GameContext Context { get; set; }
+
+        public override void Awake()
+        {
+            ComponentOwner.KeyEventPublisher.Subscribe(GetInput);
+            
+        }
+
+        public void GetInput(KeyEventArgs args)
+        {
+            if(Context is null)
+            {
+                return;
+            }
+
+            Context.PressedKeyInfo = args.KeyInfo;
+        }
+
+        public override void OnDestroy()
+        {
+            ComponentOwner.KeyEventPublisher.Unsubscribe(GetInput);
+        }
+    }
+}
