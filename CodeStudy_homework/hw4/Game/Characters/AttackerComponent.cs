2fa18cd514ef930e110b843920bd843b7fcb5252
윤석 이 <nuclearbomb280@gmail.com>
2025-09-19 00:00:24 +0900

    공격 위한 AttackerComponent추가

---
+++ CodeStudy_homework/hw4/Game/Characters/AttackerComponent.cs
@@ -0,0 +1,32 @@
+﻿using hw4.Engine.Component;
+
+namespace hw4.Game.Characters
+{
+    public class AttackerComponent : ComponentBehaviour
+    {
+        private int _strength = 0;
+        private int _killCnt = 0;
+        private int _attackCnt = 0;
+
+        public int Strength { get; set; }
+        public int KillCnt { get; }
+        public int AttackCnt { get; }
+
+        public void Attack(HPComponent hPComponent)
+        {
+            if(hPComponent.IsDead())
+            {
+                return;
+            }
+
+            _attackCnt++;
+
+            hPComponent.TakeDamage(_strength);
+
+            if(hPComponent.IsDead())
+            {
+                _killCnt++;
+            }
+        }
+    }
+}
