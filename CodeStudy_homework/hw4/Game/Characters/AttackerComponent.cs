433c53c10976b6fe29631447765e1f4ef8ce1149
윤석 이 <nuclearbomb280@gmail.com>
2025-09-19 00:16:08 +0900

    AttackerComponent에 로그 기능 추가

--- CodeStudy_homework/hw4/Game/Characters/AttackerComponent.cs
+++ CodeStudy_homework/hw4/Game/Characters/AttackerComponent.cs
@@ -4,28 +4,46 @@ namespace hw4.Game.Characters
 {
     public class AttackerComponent : ComponentBehaviour
     {
+        
         private int _strength = 0;
         private int _killCnt = 0;
         private int _attackCnt = 0;
+        private ClassComponent _myInfo;
 
         public int Strength { get; set; }
         public int KillCnt { get; }
         public int AttackCnt { get; }
+        public GameContext.GameContext GameContext { get;  set; }
 
-        public void Attack(HPComponent hPComponent)
+        public override void Start()
         {
-            if(hPComponent.IsDead())
+            _myInfo = ComponentOwner.GetComponent<ClassComponent>();
+        }
+
+        public void Attack(HPComponent HPComponent, ClassComponent classComponent)
+        {
+            if(HPComponent.IsDead())
             {
                 return;
             }
 
             _attackCnt++;
 
-            hPComponent.TakeDamage(_strength);
+            HPComponent.TakeDamage(_strength);
 
-            if(hPComponent.IsDead())
+            if(GameContext.SkipInfo == false)
+            {
+                Console.WriteLine($"{_myInfo.Name} 이 {classComponent.Name}을 공격해서 체력 {HPComponent.GetHP()}됨");
+            }
+
+            if(HPComponent.IsDead())
             {
                 _killCnt++;
+
+                if(GameContext.SkipInfo == false)
+                {
+                    Console.WriteLine($"{classComponent.Name}이 죽고, {_myInfo.Name}의 처치 횟수가 {_killCnt}로 올랐다");
+                }
             }
         }
     }
