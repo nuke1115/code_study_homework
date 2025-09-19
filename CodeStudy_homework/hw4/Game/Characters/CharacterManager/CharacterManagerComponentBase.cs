9c04ea5f5fb88dcbc7206faccccb90f5bcf4f435
윤석 이 <nuclearbomb280@gmail.com>
2025-09-20 00:52:22 +0900

    캐릭터 메니저들 추가

--- CodeStudy_homework/hw4/Game/Characters/CharacterManager/CharacterManagerComponentBase.cs
+++ CodeStudy_homework/hw4/Game/Characters/CharacterManager/CharacterManagerComponentBase.cs
@@ -5,17 +5,55 @@ namespace hw4.Game.Characters.CharacterManager
 {
     public abstract class CharacterManagerComponentBase : ComponentBehaviour
     {
-        private CharacterArrayList _characterList = new CharacterArrayList(16);
-        Random Random = new Random();
+        protected CharacterArrayList _characterList = new CharacterArrayList(16);
+
+        protected CharacterManagerComponentBase _opponentManager;
+        
+        protected Random _random = new Random();
+
+        private int _lastPickedCharacterIndex = -1;
+        public GameContext.GameContext GameContext { get; set; }
         public override void Awake()
         {
             ComponentOwner.UpdateEvent += Update;
         }
 
-        public void Init(int characterCnt)
+        protected void AddCharacter(int strength,int hp, string className, string name)
         {
-            _characterList.Clear();
+            var Character = ComponentOwner.GameObjectRequester.Instantiate<GameObject>();
+            CharacterComponentBundle bundle = new CharacterComponentBundle();
+            bundle.attackerComponent = Character.AddComponent<AttackerComponent>();
+            bundle.HPComponent = Character.AddComponent<HPComponent>();
+            bundle.classComponent = Character.AddComponent<ClassComponent>();
+
+            bundle.classComponent.Name = name;
+            bundle.classComponent.Class = className;
+
+            bundle.attackerComponent.Strength = strength;
+            bundle.attackerComponent.GameContext = GameContext;
+
+            bundle.HPComponent.SetHP(hp);
+
+            _characterList.AddAtLastAliveCharacter(bundle);
         }
+
+        public bool IsAllDead()
+        {
+            return _characterList.GetAliveCount() < 1;
+        }
+
+        public CharacterComponentBundle GetCharacter()
+        {
+            _lastPickedCharacterIndex = _random.Next(0, _characterList.GetAliveCount());
+            return _characterList[_lastPickedCharacterIndex];
+        }
+        
+        public void OnCharacterDeath()
+        {
+            _characterList.CondenseAliveCharacter(_lastPickedCharacterIndex);
+        }
+
+        public abstract void Init(int characterCnt);
 
         public override void OnDestroy()
         {
---
+++ CodeStudy_homework/hw4/Game/Characters/CharacterManager/UnitManagerComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4.Game.Characters.CharacterManager
+{
+    public class UnitManagerComponent : CharacterManagerComponentBase
+    {
+        public override void Start()
+        {
+            _opponentManager = ComponentOwner.GameObjectRequester.GetGameObject<GameObject>((int)eNames.MonsterManager).GetComponent<MonsterManagerComponent>();
+        }
+
+        public override void Init(int characterCnt)
+        {
+            _characterList.Clear();
+
+            for(int i = 0; i < characterCnt; i++)
+            {
+                switch(_random.Next(0,3))
+                {
+                    case 0:
+                        AddCharacter(_random.Next(20, 31), _random.Next(30, 41), "Knight", "철(수저)기사");
+                        break;
+                    case 1:
+                        AddCharacter(_random.Next(15, 26), _random.Next(30, 38), "Archer", "대한민국의 양궁 국가대표");
+                        break;
+                    case 2:
+                        AddCharacter(_random.Next(10, 25), _random.Next(20, 26), "Magician", "흙흙마법사");
+                        break;
+                }
+            }
+        }
+        public override void Update()
+        {
+            if(GameContext.GameState == Game.GameContext.eGameStates.UNIT_TURN)
+            {
+                var unit = _characterList[_random.Next(0, _characterList.GetAliveCount())];
+                var monster = _opponentManager.GetCharacter();
+                unit.attackerComponent.Attack(monster.HPComponent, monster.classComponent, _opponentManager.OnCharacterDeath);
+            }
+        }
+    }
+}
