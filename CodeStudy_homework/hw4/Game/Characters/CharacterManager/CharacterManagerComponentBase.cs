bb6b144da90d9df7ecdb4ff7a54e0227d2148c26
윤석 이 <nuclearbomb280@gmail.com>
2025-09-19 23:59:22 +0900

    CharacterManagerComponent들의 베이스 클래스 추가

---
+++ CodeStudy_homework/hw4/Game/Characters/CharacterManager/CharacterManagerComponentBase.cs
@@ -0,0 +1,26 @@
+﻿using hw4.Engine.Component;
+using hw4.Game.DataStructure;
+
+namespace hw4.Game.Characters.CharacterManager
+{
+    public abstract class CharacterManagerComponentBase : ComponentBehaviour
+    {
+        private CharacterArrayList _characterList = new CharacterArrayList(16);
+        Random Random = new Random();
+        public override void Awake()
+        {
+            ComponentOwner.UpdateEvent += Update;
+        }
+
+        public void Init(int characterCnt)
+        {
+            _characterList.Clear();
+        }
+
+        public override void OnDestroy()
+        {
+            ComponentOwner.UpdateEvent -= Update;
+            _characterList.Clear();
+        }
+    }
+}
